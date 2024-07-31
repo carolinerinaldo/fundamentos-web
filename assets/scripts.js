e0dcd968e2040ed493b8b98d73e324f8c910e80a
Caroline <carolinebauru@hotmail.com>
2024-07-31 19:42:30 -0300

    css

---
+++ assets/scripts.js
@@ -0,0 +1,11 @@
+function hello(){
+    alert("Hello World")
+}
+
+let nome = document.getElementById("nome"); // conectada ao input
+
+function exibeNome(){
+    alert(nome.value);
+}
+
+console.log(nome.value);
